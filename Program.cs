51009fcdcb741672c9afca6ea9716ba86684b394
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-14 09:04:31 +0700

    Constructors

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Potter", "JKRowling",400);
+            Book book2 = new Book("Lord Angga", "Tolkeir", 700);
+            Book book3 = new Book();
+            book2.title = "The Habbit";
+
+            Console.WriteLine(book2.title);
+            Console.ReadLine();
+        }
+    }
+}
